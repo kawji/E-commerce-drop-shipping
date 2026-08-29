ebdc2a4f3941e8a40c04500e5219a38f9e7c675d
kawji <24.supavich@gmail.com>
2026-08-29 21:24:41 +0700

    add item card product layout design

--- src/layout/feedpage.tsx
+++ src/layout/feedpage.tsx
@@ -7,6 +7,9 @@ import AccountIcon from '@/components/icons/account';
 import BtnNormal from '@/components/btnNormal';
 import BtnSort from '@/components/btnSort';
 import Image from 'next/image';
+import Staricon from '@/components/icons/star';
+
+
 
 type databaseItemsType = [string,string,number][]
 
@@ -24,7 +27,7 @@ export default function Feedpage() {
     ]
 
     return(
-        <div className="flex flex-col w-full items-center ">
+        <div className="flex flex-col w-full items-center mb-50 ">
             
             <nav className="flex item-center justify-center w-full bg-[#0f3612] ">
                 <div className='flex items-center justify-between w-full max-w-380 py-2 text-white/83 text-[13px] '>
@@ -112,6 +115,64 @@ export default function Feedpage() {
                     <ChevronDown size={15} className='text-black font-medium' />
                 </button>
  
+            </div>
+
+            <div className='w-full max-w-380 flex items-center font-bold text-xl mt-6 text-black/90 '>
+                <p>Headphones For You!</p>                
+            </div>
+            <div className='grid grid-cols-6 w-full max-w-380 gap-4 mt-5 '>
+
+                <div className='flex flex-col items-center aspect-1/1.25 gap-3 hover:shadow hover:scale-101 cursor-pointer transition-all duration-300 p-2 rounded '>
+                    <div className=' relative w-full aspect-1.5/1.25 flex items-center justify-center bg-black/5 rounded-md '>
+                        <Image 
+                        src="/pg/headphone1-.png"
+                        width={180}
+                        height={180}
+                        className='object-cover '
+                        alt="Picture of the headphon"
+                        />
+
+                    </div>
+                    <div className='flex-1 w-full flex flex-col  '>
+                        <div className='flex flex-col w-full gap-1'>
+                            <div className='flex items-center justify-between text-base  font-bold text-black/88 '>
+                                <p className=' leading-none '>Bose BT Earphones</p>
+                                <span className='flex text-[11px] '>
+                                    <p>$</p>
+                                    <p className='text-base'>283</p>
+                                    <p>.00</p>
+                                </span>
+                            </div>
+                            <div className='text-[11px] text-black/70 font-semibold '>
+                                Table with air purifier, stained venner/black
+                            </div>
+                            <div className='flex gap-1 '>
+                                <Staricon className='text-green-500' />
+                                <Staricon className='text-green-500' />
+                                <Staricon className='text-green-500' />
+                                <Staricon className='text-green-500' />
+                                <Staricon className='text-green-500' />
+                                <p className='text-[12px] ml-1 text-black/77 '>(121)</p>
+                            </div>
+                            <div className='w-full flex items-center mt-1'>
+                                <button className='px-3.5 py-1.5 border rounded-3xl text-[12px] font-semibold tracking-wide cursor-pointer border-[#0f3612] bg-zince-50 hover:bg-[#0f3612] hover:text-white/93 text-black/90  transition-colors  '>Add to Cart</button>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                
+
+                </div>
+
+
+
+
+
+
+
+
             </div>
 
 
